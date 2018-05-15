0b43d839499e5b23f6123247507153307dd16111
sophiegri <sophiegrimme@gmx.de>
2018-05-15 15:38:29 +0200

    Aufgabe 3.2 vollständig

---
+++ build/source/CMakeFiles/aufgabe_2_und_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aufgabe_2_und_3.dir/aufgabe_2_und_3.cpp.o"
+  "aufgabe_2_und_3.pdb"
+  "aufgabe_2_und_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aufgabe_2_und_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
