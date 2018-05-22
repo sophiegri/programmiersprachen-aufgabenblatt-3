db37638dcd2c0646b840da61808dfb16592dc2d0
sophiegri <sophiegrimme@gmx.de>
2018-05-22 19:30:19 +0200

    finish task 10

---
+++ source/aufgabe_10.cpp
@@ -0,0 +1,35 @@
+#include <list> // std::list <>
+#include <cstdlib> // std::rand ()
+#include <iostream> // std::cout 
+#include <vector> // std::vector <>
+#include <iterator> // std::ostream_iterator <>
+using namespace std; 
+
+
+int main ()
+{
+
+//Aufgabe 3.10
+
+std::list <unsigned int> L (100); //Liste aus Aufgabe 3.2 
+for (auto& l : L) 
+{ 
+    l = std::rand() % 100 + 1;
+}
+
+std::vector <int> v_0 (100); //Es wird ein neuer Vektor angelegt 
+ 
+
+std::copy (L.begin() , L.end(), v_0.begin() ); //Die Elemente aus der Liste werden in den Vektor eingefügt
+std::cout << "My vector contains:"; 
+    for (std::vector<int>::iterator it = v_0.begin(); it!=v_0.end(); ++it) //Ausgabe mit Iterator it 
+    {
+        std::cout << ' ' << *it; 
+        std::cout << '\n'; 
+    }
+
+return 0;
+
+}
+
+
