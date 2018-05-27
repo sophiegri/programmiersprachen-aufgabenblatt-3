f15bf9e9ee07880838c87668e5219cb24e705854
sophiegri <sophiegrimme@gmx.de>
2018-05-27 13:21:15 +0200

    finish task 12

---
+++ source/aufgabe_12.cpp
@@ -0,0 +1,35 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <vector> // std::vector <>
+#include <iostream> 
+#include <cmath>
+#include <algorithm>
+
+std::vector<int> v_1 {1,2,3,4,5,6,7,8,9};
+std::vector<int> v_2 {9,8,7,6,5,4,3,2,1};
+std::vector<int> v_3(9);
+
+bool is_10 (int i) //Funktion ob es gleich 10 ist 
+{
+  return i == 10 ; 
+}
+
+TEST_CASE ("pruefe ob alles 10 ist." "[transform]")
+{
+    REQUIRE(std::all_of(v_3.begin(), v_3.end(), is_10));
+}
+
+int main(int argc, char* argv[])
+{    
+  auto addition = [] (int x, int y) -> int {return x + y;}; 
+  //auto wichtig, Bindung weist auf Lambda hin
+  
+  std::transform (v_1.begin(), v_1.end(), v_2.begin(), v_3.begin(), addition);
+  
+  for(auto& i : v_3) 
+  {
+    std::cout << i << "\n";
+  }
+
+  return Catch::Session().run(argc, argv);
+} 
