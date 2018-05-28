bcb3a313cfec36b0a0b8c3116a45e6254297b6e7
sophiegri <sophiegrimme@gmx.de>
2018-05-28 15:55:49 +0200

    start task 14, not solved yet

---
+++ source/aufgabe_14.cpp
@@ -0,0 +1,29 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include "circle.hpp"
+#include "aufgabe_4.cpp"
+#include <iostream>
+#include <algorithm> 
+#include <vector> 
+
+
+//Aufgabe 3.14
+
+
+
+//vector <Circle> sorted_circles{c_1,c_2,c_3};
+ 
+TEST_CASE ("Lambda-Test größer 4.0f")
+{
+std::vector<Circle> circles{{5.0f}, {3.0f}, {8.0f}, {1.0f}, {5.0f}};
+std::cout << "Radien: " << circles << "\n"; 
+//REQUIRE();
+} 
+
+
+int main(int argc, char *argv[])
+{ 
+
+  return Catch::Session().run(argc, argv); //Für den Test Case
+}
+
